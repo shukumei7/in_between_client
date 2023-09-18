cf96e9cabd951246a85a4ced301cdef74730083c
Allan Bate <allbate@gmail.com>
2023-09-17 17:31:01 -0700

    fixed timer

--- js/IB/timer.js
+++ js/IB/timer.js
@@ -3,34 +3,40 @@ import IBC from '../ib.js';
 const e = React.createElement;
 
 export function Timer({deadline}) {
+    const [ display , setDisplay ]  = React.useState('');
     const [ time , setTime ] = React.useState(0);
-    const [ timed, setTimed ] = React.useState(0);
 
     React.useEffect(() => { // initialize timer
         const diff = deadline - (Date.now() / 1000);
         // console.log('Start Timer', deadline, diff);
-        setTimed(diff);
+        setTime(IBC.timeout - diff);
         setTimeout(() => {
             $('.sand').css('transition', 'top 1s linear');
         }, 200);
+        if(diff <= 0) {
+            return;
+        }
         IBC.play('bell');
     }, []);
 
     React.useEffect(() => {
+        const diff = IBC.timeout - time;
         setTimeout(() => {
-            setTime(time + 1);
-            if(time == Math.round(IBC.timeout / 2)) {
+            const next = time + 1;
+            setTime(next);
+            if(next == Math.round(IBC.timeout / 2)) {
                 console.log('Half time alert', time, IBC.timeout);
                 IBC.play('bell');
-            } else if(timed > 0 && time < timed && timed - time < IBC.alertDiff) { // last few seconds
-                console.log('Last ' + (timed - time) + 'seconds');
+            } else if(diff >= 0 && diff < IBC.alertDiff) { // last few seconds
+                console.log('Last ' + (IBC.timeout - next) + 'seconds');
                 IBC.play('bell');
             }
+            const percentage = diff < 0 ? 100 : next / IBC.timeout * 100;
+            setDisplay(e('div', { key  : 'timer' , className : 'timer' }, e('div', { key : 'sand' , className : 'sand', style : { top : percentage + '%'}}, ' ')))
         }, 1000);
     });
-    // console.log('Render Timer', time, timed);
-    const percentage = timed < 0 ? 100 : time / timed * 100;
-    return e('div', { key  : 'timer' , className : 'timer' }, e('div', { key : 'sand' , className : 'sand', style : { top : percentage + '%'}}, ' '));
+
+    return display;
 }
 
 export default Timer;
