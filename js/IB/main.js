8dd39288e09f1812395a3c0df5e7bbc632ac795a
Allan Bate <allbate@gmail.com>
2023-10-02 17:34:52 -0700

    full screen

--- js/IB/main.js
+++ js/IB/main.js
@@ -50,6 +50,14 @@ function IBCMain() {
         IBC.play('tick');
         IBC.play('lounge', 'bgm');
         setInteraction(true);
+        const elem = document.documentElement;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
     }
 
     const updateGameData = (status, limited) => {
